b5a5cf3206b8758edefa2424f14aeaf7d570e636
Deepak <deepakworkpc@gmail.com>
2024-06-02 17:35:43 +0530

    all test cases pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) {
+        return "ERROR";
+    }
+    let newString = str.repeat(count);
+    // console.log(newString)
+    return newString;
 };
 
+repeatString("hey", -1)
 // Do not edit below this line
 module.exports = repeatString;
